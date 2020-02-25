c7f2c97bafcf9e3b47e2a893537a139691b8c76c
swapna <swapna1@gmail.com>
2020-02-24 22:35:12 -0500

    Added System.Out message

--- HelloGit/src/com/test/git/HelloGit.java
+++ HelloGit/src/com/test/git/HelloGit.java
@@ -3,8 +3,9 @@ package com.test.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git");
-
+		System.out.println("Hello Git from master");
+		int i =2;
+		System.out.println("i: " + i);
 	}
 
 }
